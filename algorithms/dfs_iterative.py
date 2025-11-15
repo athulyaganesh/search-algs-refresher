9f330d0f9e9c2bd74fe1c48afd272eea7105e36a
Athulya Ganesh <athulya.ganesh2001@gmail.com>
2025-11-14 21:54:07 -0500

    dfs

--- algorithms/dfs_iterative.py
+++ algorithms/dfs_iterative.py
@@ -1,2 +1,24 @@
-def dfs_iterative():
-    return 
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+    } 
+
+def dfs_iterative(graph, start):
+    stack = [start]
+    visited = set() 
+    
+    while stack: 
+        currentNode = stack.pop()
+        if currentNode in visited: 
+            continue  
+        visited.add(currentNode) 
+        print(currentNode)
+        for n in graph[currentNode]: 
+            if n not in visited and n not in stack: 
+                stack.append(n) 
+                
+dfs_iterative(graph, 'A')
--- algorithms/dfs_recusive.py
+++ algorithms/dfs_recusive.py
@@ -1,1 +1,24 @@
 #DFS: Recursive Implementation in Python
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+    } 
+
+def dfs_recursive(graph, node, visited): 
+    if visited is None: 
+        visited = set()
+    
+    visited.add(node)
+    print(node)
+    
+    for n in graph[node]:
+        if n not in visited: 
+            dfs_recursive(graph, n, visited)
+    
+    return visited 
+
+dfs_recursive(graph, 'A', set())
