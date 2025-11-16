db50de97473625277bb6d7d46b805898cb45cd77
Athulya Ganesh <athulya.ganesh2001@gmail.com>
2025-11-15 20:37:42 -0500

    BFS

--- algorithms/bfs_iterative.py
+++ algorithms/bfs_iterative.py
@@ -1,2 +1,26 @@
-def bfs_iterative():
-    return 
+def bfs_iterative(graph, start):
+    visited = set() 
+    queue = [start] 
+    
+    while queue: 
+        curNode = queue.pop(0)
+        if curNode in visited: 
+            continue 
+        visited.add(curNode)
+        print(curNode)
+        
+        for node in graph[curNode]: 
+            if node not in queue and node not in visited: 
+                queue.append(node)
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+    } 
+
+bfs_iterative(graph, 'A')           
+        
--- algorithms/bfs_recursive.py
+++ algorithms/bfs_recursive.py
@@ -1,2 +1,27 @@
-def bfs_recursive():
-    return 
+def bfs_recursive(graph, queue, visited):  
+    if not queue: 
+        return 
+    if not visited: 
+        return 
+    
+    curNode = queue.pop(0)
+    visited.add(curNode)
+    print(curNode)
+    
+    for n in graph[curNode]: 
+        queue.append(n)
+    
+    bfs_recursive(graph, queue, visited)
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+    } 
+
+start = 'A'
+bfs_recursive(graph, [start], {start})           
+        
