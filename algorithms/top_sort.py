4bc1eef1e50976ca299493a4d99ea7b5457f643b
Athulya Ganesh <athulya.ganesh2001@gmail.com>
2025-11-19 20:14:45 -0500

    more algs

--- algorithms/top_sort.py
+++ algorithms/top_sort.py
@@ -1,2 +1,41 @@
-def top_sort():
-    return
+def top_sort(graph):
+    indegrees = {node : 0 for node in graph}
+    
+    for node in graph: 
+        nei = graph[node] 
+        for n in nei: 
+            indegrees[n] += 1 
+    
+    q = [] 
+    res = [] 
+    count = 0
+    
+    for node in graph: 
+        if indegrees[node] == 0: 
+            q.append(node)
+    
+    while len(q): 
+        curNode = q.pop(0)
+        
+        res.append(curNode) 
+        count += 1 
+        
+        for n in graph[curNode]: 
+            indegrees[n] -= 1 
+            if indegrees[n] == 0: 
+                q.append(n) 
+    
+    if len(res) != count: 
+        return "CYCLE DETECTED"
+    return res 
+
+
+graph = {
+    'A' : ['B', 'C'], 
+    'B' : ['D'], 
+    'C' : ['D'], 
+    'D' : ['E'], 
+    'E' : [] 
+}
+
+print(top_sort(graph))
--- algorithms/union_find.py
+++ algorithms/union_find.py
@@ -1,2 +1,31 @@
-def union_find():
-    return 
+class DisjointSet: 
+    def __init__(self, n): 
+        self.parent = list(range(n))
+        self.rank = [0] * n
+    
+    def find(self, i): 
+        if i == self.parent[i]: 
+            return i 
+        self.parent[i] = self.find(self.parent[i])
+        return self.parent[i] 
+
+    def union(self, i, j): 
+        i = self.find(i)
+        j = self.find(j)
+        
+        if i == j: 
+            return False 
+        
+        if self.rank[i] < self.rank[j]: 
+            self.parent[i] = j 
+        elif self.rank[i] > self.rank[j]: 
+            self.parent[j] = i 
+        else: 
+            self.parent[i] = j 
+            self.rank[i] += 1
+        return True 
+    
+    def intersect(self, i, j): 
+        return self.find(i) == self.find(j)
+
+s = DisjointSet(5) 
